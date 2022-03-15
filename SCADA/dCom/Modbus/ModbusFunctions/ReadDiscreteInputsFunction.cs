7f0245bb8f37eed201fb548d0f6b515fecab7dac
Dusan-Ivic <ivicd99@gmail.com>
2022-03-15 21:17:46 +0100

    Vezba 2

--- SCADA/dCom/Modbus/ModbusFunctions/ReadDiscreteInputsFunction.cs
+++ SCADA/dCom/Modbus/ModbusFunctions/ReadDiscreteInputsFunction.cs
@@ -24,15 +24,55 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] req = new byte[12];
+
+            req[0] = BitConverter.GetBytes(CommandParameters.TransactionId)[1];
+            req[1] = BitConverter.GetBytes(CommandParameters.TransactionId)[0];
+            req[2] = BitConverter.GetBytes(CommandParameters.ProtocolId)[1];
+            req[3] = BitConverter.GetBytes(CommandParameters.ProtocolId)[0];
+            req[4] = BitConverter.GetBytes(CommandParameters.Length)[1];
+            req[5] = BitConverter.GetBytes(CommandParameters.Length)[0];
+            req[6] = CommandParameters.UnitId;
+            req[7] = CommandParameters.FunctionCode;
+            req[8] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).StartAddress)[1];
+            req[9] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).StartAddress)[0];
+            req[10] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).Quantity)[1];
+            req[11] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).Quantity)[0];
+
+            return req;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> resp = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int byteCount = response[8];
+            ushort startAddress = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+            ushort counter = 0;
+
+            for (int i = 0; i < byteCount; i++)
+            {
+                byte temp = response[9 + i];
+                byte mask = 1;
+
+                ushort quantity = ((ModbusReadCommandParameters)CommandParameters).Quantity;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    ushort value = (ushort)(temp & mask);
+                    Tuple<PointType, ushort> tuple = new Tuple<PointType, ushort>(PointType.DIGITAL_INPUT, startAddress++);
+                    resp.Add(tuple, value);
+                    
+                    temp >>= 1;
+                    counter++;
+
+                    if (counter >= quantity)
+                        break;
+                }
+            }
+
+            return resp;
         }
     }
 }
