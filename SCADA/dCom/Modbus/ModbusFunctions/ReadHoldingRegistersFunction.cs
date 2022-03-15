af1cbe12d5135edd0a59cc7fa941fb74d2b8b760
Dusan-Ivic <ivicd99@gmail.com>
2022-03-15 21:06:56 +0100

    Vezba 1

--- SCADA/dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ SCADA/dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -24,15 +24,40 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] req = new byte[12];
+
+            req[0] = BitConverter.GetBytes(CommandParameters.TransactionId)[1];
+            req[1] = BitConverter.GetBytes(CommandParameters.TransactionId)[0];
+            req[2] = BitConverter.GetBytes(CommandParameters.ProtocolId)[1];
+            req[3] = BitConverter.GetBytes(CommandParameters.ProtocolId)[0];
+            req[4] = BitConverter.GetBytes(CommandParameters.Length)[1];
+            req[5] = BitConverter.GetBytes(CommandParameters.Length)[0];
+            req[6] = CommandParameters.UnitId;
+            req[7] = CommandParameters.FunctionCode;
+            req[8] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).StartAddress)[1];
+            req[9] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).StartAddress)[0];
+            req[10] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).Quantity)[1];
+            req[11] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).Quantity)[0];
+
+            return req;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> resp = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int byteCount = response[8];
+            ushort startAddress = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            for (int i = 0; i < byteCount; i += 2)
+            {
+                ushort value = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 9 + i));
+                Tuple<PointType, ushort> tuple = new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, startAddress++);
+                resp.Add(tuple, value);
+            }
+
+            return resp;
         }
     }
 }
