0adfcb0333c85490ba0217a80f28e55ee4b17a7c
Dusan-Ivic <ivicd99@gmail.com>
2022-05-11 18:59:22 +0200

    Vezba 6

--- SCADA/dCom/ProcessingModule/AutomationManager.cs
+++ SCADA/dCom/ProcessingModule/AutomationManager.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ProcessingModule
@@ -60,10 +61,40 @@ namespace ProcessingModule
 
 		private void AutomationWorker_DoWork()
 		{
-			//while (!disposedValue)
-			//{
-			//}
-		}
+            while (!disposedValue)
+            {
+                EGUConverter eguConverter = new EGUConverter();
+
+                PointIdentifier analogOut = new PointIdentifier(PointType.ANALOG_OUTPUT, 1000);
+                PointIdentifier digitalOut1 = new PointIdentifier(PointType.DIGITAL_OUTPUT, 2000);
+                PointIdentifier digitalOut2 = new PointIdentifier(PointType.DIGITAL_OUTPUT, 2001);
+
+                List<PointIdentifier> pointList = new List<PointIdentifier> { analogOut, digitalOut1, digitalOut2 };
+                List<IPoint> points = storage.GetPoints(pointList);
+
+                if (points[1].RawValue == 1)
+                {
+                    int value = (int)eguConverter.ConvertToEGU(points[0].ConfigItem.ScaleFactor, points[0].ConfigItem.Deviation, points[0].RawValue);
+                    value -= 10;
+                    processingManager.ExecuteWriteCommand(points[0].ConfigItem, configuration.GetTransactionId(), configuration.UnitAddress, points[0].ConfigItem.StartAddress, value);
+                }
+
+                if (points[2].RawValue == 1)
+                {
+                    int value = (int)eguConverter.ConvertToEGU(points[0].ConfigItem.ScaleFactor, points[0].ConfigItem.Deviation, points[0].RawValue);
+                    value -= 15;
+                    processingManager.ExecuteWriteCommand(points[0].ConfigItem, configuration.GetTransactionId(), configuration.UnitAddress, points[0].ConfigItem.StartAddress, value);
+                }
+
+                if (points[0].Alarm == AlarmType.LOW_ALARM)
+                {
+                    processingManager.ExecuteWriteCommand(points[1].ConfigItem, configuration.GetTransactionId(), configuration.UnitAddress, points[1].ConfigItem.StartAddress, 0);
+                    processingManager.ExecuteWriteCommand(points[2].ConfigItem, configuration.GetTransactionId(), configuration.UnitAddress, points[2].ConfigItem.StartAddress, 0);
+                }
+
+                automationTrigger.WaitOne(delayBetweenCommands);
+            }
+        }
 
 		#region IDisposable Support
 		private bool disposedValue = false; // To detect redundant calls
